6b9dbdd07a0c1720d47e1cbeffd5b356c94d15e5
Pujago <govilpuja@yahoo.com>
2020-09-20 22:30:58 +1200

    Update installwebserver.ps1

--- installwebserver.ps1
+++ installwebserver.ps1
@@ -1,2 +1,5 @@
 ﻿Import-Module servermanager
 Add-Windowsfeature web-server -includeallsubfeature
+
+$file = "C:\inetpub\wwwroot\default.html"
+New-Item $file -ItemType File -Value "Welcome to demovm."
